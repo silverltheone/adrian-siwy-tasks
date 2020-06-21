b8ca8014fb941f57f43acef14f7edede80bf03fb
silverltheone <adeksiwy@gmail.com>
2020-06-21 12:28:20 +0200

    32.2

--- src/main/java/com/crud/tasks/config/CompanyConfig.java
+++ src/main/java/com/crud/tasks/config/CompanyConfig.java
@@ -1,16 +1,13 @@
 package com.crud.tasks.config;
 
-
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Getter
 @Component
-public class AdminConfig {
-    @Value("${admin.mail}")
-    private String adminMail;
+public class CompanyConfig {
 
-    @Value("${admin.name}")
-    private String adminName;
+    @Value("${info.company.name}")
+    private String companyName;
 }
